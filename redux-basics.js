d881f8792d49e4ab6a03699f4fa5ebe7f3090acb
Manpreet Singh <manpreet.singh10196@gmail.com>
2020-04-17 11:33:58 +0530

    add redux basic.js

---
+++ redux-basics.js
@@ -0,0 +1,19 @@
+const redux = require('redux');
+const createStore = redux.createStore;
+
+const initialState ={ 
+    counter: 0
+}
+// Reducer
+const rootReducer = (state = initialState, action) => {
+    return state;
+}
+
+// Store
+const store= createStore(rootReducer);
+console.log(store.getState());
+
+//Dispatching Action
+
+
+// Subscription

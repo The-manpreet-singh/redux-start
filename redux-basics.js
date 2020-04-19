6eff7a38a1c6c1de0fc198d52e1c181a252b788e
Manpreet Singh <manpreet.singh10196@gmail.com>
2020-04-19 10:56:54 +0530

    complete dispatch

--- redux-basics.js
+++ redux-basics.js
@@ -6,6 +6,19 @@ const initialState ={
 }
 // Reducer
 const rootReducer = (state = initialState, action) => {
+   if(action.type === 'INC_COUNTER') {
+      return {
+          ...state,
+          counter: state.counter + 1
+      }
+   }
+   if(action.type === 'ADD_COUNTER') {
+    return {
+        ...state,
+        counter: state.counter + action.value
+    }
+ }
+   
     return state;
 }
 

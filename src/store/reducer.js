cb2ef3ccd3b685b77a2d4c0c976eb91d2bfb8d34
Manpreet Singh <manpreet.singh10196@gmail.com>
2020-04-19 13:51:32 +0530

    add switch case instead of if

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -3,10 +3,29 @@ const initialState = {
 };
 
 const reducer = (state = initialState, action) => {
-  if (action.type === "INCREMENT") {
+  switch (action.type) {
+    case "INCREMENT":
+      return {
+        counter: state.counter + 1,
+      };
+    case "DECREMENT":
+      return {
+        counter: state.counter - 1,
+      };
+    case "ADD":
+      return {
+        counter: state.counter + action.val,
+      };
+    case "SUBTRACT":
+      return {
+        counter: state.counter - action.val,
+      };
+  }
+
+  /*  if (action.type === "INCREMENT") {
     return {
-      counter: state.counter + 1
-    };
+        counter: state.counter + 1
+      }
   }
   if (action.type === "DECREMENT") {
     return {
@@ -22,7 +41,7 @@ const reducer = (state = initialState, action) => {
     return {
       counter: state.counter - action.val
     };
-  }
+  } */
   return state;
 };
 

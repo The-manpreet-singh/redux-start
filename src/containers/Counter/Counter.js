a1f61fc02f186221b3d2452588445e1680190f2a
Manpreet Singh <manpreet.singh10196@gmail.com>
2020-04-19 13:41:15 +0530

    passing the retrive data

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -31,9 +31,9 @@ class Counter extends Component {
             <div>
                 <CounterOutput value={this.props.ctr} />
                 <CounterControl label="Increment" clicked={this.props.onIncrementCounter} />
-                <CounterControl label="Decrement" clicked={() => this.counterChangedHandler( 'dec' )}  />
-                <CounterControl label="Add 5" clicked={() => this.counterChangedHandler( 'add', 5 )}  />
-                <CounterControl label="Subtract 5" clicked={() => this.counterChangedHandler( 'sub', 5 )}  />
+                <CounterControl label="Decrement" clicked={this.props.onDecrementCounter}  />
+                <CounterControl label="Add 10" clicked={this.props.onAddCounter}  />
+                <CounterControl label="Subtract 15" clicked={this.props.onSubtractCounter}  />
             </div>
         );
     }
@@ -47,7 +47,10 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
     return {
-        onIncrementCounter: () => dispatch({type: 'INCREMENT'})
+        onIncrementCounter: () => dispatch({type: 'INCREMENT'}),
+        onDecrementCounter: () => dispatch({type: 'DECREMENT'}),
+        onAddCounter: () => dispatch({type: 'ADD', val:10}),
+        onSubtractCounter: () => dispatch({type: 'SUBTRACT', val:15}),
     }
 }
 
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,14 +1,29 @@
-  const initialState = {
-      counter: 0
+const initialState = {
+  counter: 0,
+};
+
+const reducer = (state = initialState, action) => {
+  if (action.type === "INCREMENT") {
+    return {
+      counter: state.counter + 1
+    };
   }
+  if (action.type === "DECREMENT") {
+    return {
+      counter: state.counter - 1
+    };
+  }
+  if (action.type === "ADD") {
+    return {
+      counter: state.counter + action.val
+    };
+  }
+  if (action.type === "SUBTRACT") {
+    return {
+      counter: state.counter - action.val
+    };
+  }
+  return state;
+};
 
-  const reducer = ( state= initialState, action) => {
-      if(action.type === 'INCREMENT') {
-           return {
-               counter: state.counter +1
-           }
-      }
-      return state;
-  };
-
-  export default reducer;
+export default reducer;

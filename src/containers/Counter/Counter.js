fd1fc559c8a8e3e3ce6d1d207b855c13e97325db
Manpreet Singh <manpreet.singh10196@gmail.com>
2020-04-20 12:10:02 +0530

    combine two reducers

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 
 import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
-import * as actionTypes from '../../store/action';
+import * as actionTypes from '../../store/actions';
 
 class Counter extends Component {
     state = {
@@ -36,7 +36,7 @@ class Counter extends Component {
                 <CounterControl label="Add 10" clicked={this.props.onAddCounter}  />
                 <CounterControl label="Subtract 15" clicked={this.props.onSubtractCounter}  />
                 <hr />
-                <button onClick={this.props.onStoreResult}>Store Result</button>
+                <button onClick={()=> this.props.onStoreResult(this.props.ctr)}>Store Result</button>
                 <ul>
                     {this.props.storedResults.map(strResult => (
                       <li key={strResult.id} onClick={()=>this.props.onDeleteResult(strResult.id)}>{strResult.value}</li>
@@ -50,8 +50,8 @@ class Counter extends Component {
 
 const mapStateToProps = state => {
     return {
-        ctr: state.counter,
-        storedResults: state.results
+        ctr: state.ctr.counter,
+        storedResults: state.res.results
     };
 };
 
@@ -61,7 +61,7 @@ const mapDispatchToProps = dispatch => {
         onDecrementCounter: () => dispatch({type:  actionTypes.DECREMENT}),
         onAddCounter: () => dispatch({type:  actionTypes.ADD, val:10}),
         onSubtractCounter: () => dispatch({type:  actionTypes.SUBTRACT, val:15}),
-        onStoreResult: () => dispatch({type:  actionTypes.STORE_RESULT}),
+        onStoreResult: (result) => dispatch({type:  actionTypes.STORE_RESULT, result: result}),
         onDeleteResult: (id) => dispatch({type:  actionTypes.DELETE_RESULT, resultEId: id})
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
 
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import reducer from './store/reducer';
 
-const store = createStore(reducer);
+import counterReducer from './store/reducers/counter';
+import resultReducer from './store/reducers/result';
+
+const rootReducer = combineReducers({
+    ctr:counterReducer,
+    res:resultReducer
+});
+
+const store = createStore(rootReducer);
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
-registerServiceWorker();
+registerServiceWorker(); 
